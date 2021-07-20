39a7850d0e0eac6d8ea9755c82993922a183c79c
mariasharmagbay <mariasharmagbay@gmail.com>
2021-07-20 10:10:54 -0400

    Add project files.

---
+++ IsItALeapYear3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace IsItALeapYear3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Sharmin!");
+        }
+
+        public Boolean IsItALeapYear(int yr)
+        {
+            if (yr % 400 == 0)
+            {
+                return true;
+            }
+            else if (yr % 4 == 0)
+            {
+
+                if (yr % 100 != 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
